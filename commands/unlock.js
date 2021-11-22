b20bbb637b9ae2dc7c445d4818e2b939e4bf0b04
ItsNoah <58492926+TheDevNoah@users.noreply.github.com>
2021-11-22 12:17:07 -0600

    Updated to Djs V13

---
+++ commands/unlock.js
@@ -0,0 +1,42 @@
+const config = require("../config.json");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+module.exports = {
+  name: "unlock", // Command Name
+  aliases: ["unlock-channel"], // Aliases
+  usage: " ", // Usage
+  cooldown: 0, // Cooldown In Seconds
+  guildOnly: false, // If Command In DMs return
+  admin: false, // Bot Admin only
+  async execute(message, args, client) {    
+      let prefix = config.prefix
+      
+   if (!message.member.permissions.has("MANAGE_CHANNELS")) return message.channel.send("**`[-> You don't have the permission to preform this action! <-]`**");
+
+     const type = message.channel.type === 'text' ? 'SEND_MESSAGES' : 'CONNECT';
+message.channel.overwritePermissions([
+  {
+     id: message.guild.roles.everyone,
+     allow: ['SEND_MESSAGES', 'ADD_REACTIONS'],
+  },
+]);
+    return message.channel.send(`Channel Unlocked, ${message.author.tag}`)
+  
+  /* If you want a embed use this and replace "return message.reply(`Channel Locked, ${message.author.tag}`)" with the following:
+  
+  
+  const success = new Discord.MessageEmbed()
+  .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic:true }))
+  .setThumbnail(message.guild.iconURL())
+  .setDescription(`<#${message.channel.id}> has been unlocked by <@${message.author.id}> !`)
+  .setFooter(`Made by: ItsNoah#3408`)
+  message.reply({ embeds: [success] })
+ message.react('✅');
+  
+  */
+  
+  
+  }
+}
